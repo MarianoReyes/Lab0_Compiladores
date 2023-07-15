5b275ff70b23c5aad4d5d2a5c97bb218dfd1620d
MarianoReyes <josemarianoreyesh02@gmail.com>
2023-07-15 15:27:44 -0600

    visual

--- ejemplo.py
+++ ejemplo.py
@@ -1,9 +1,11 @@
+from anytree.exporter import DotExporter
 from antlr4 import *
 from SimpleMathLexer import SimpleMathLexer
 from SimpleMathParser import SimpleMathParser
+from anytree import Node, RenderTree
 
 # Obtén la entrada del usuario
-input_stream = InputStream(input('=> '))
+input_stream = InputStream(input('? '))
 
 # Crea un lexer con la entrada
 lexer = SimpleMathLexer(input_stream)
@@ -19,3 +21,28 @@ tree = parser.expr()
 
 # Imprime el árbol de sintaxis (para depuración)
 print(tree.toStringTree(recog=parser))
+
+# Función recursiva para construir el árbol anytree
+
+
+def build_anytree(node, antlr_node):
+    if isinstance(antlr_node, TerminalNode):
+        value = antlr_node.getText()
+        Node(value, parent=node)
+    else:
+        rule_name = parser.ruleNames[antlr_node.getRuleIndex()]
+        child_node = Node(rule_name, parent=node)
+        for child in antlr_node.getChildren():
+            build_anytree(child_node, child)
+
+
+# Construye el árbol anytree a partir del árbol de sintaxis
+root = Node(parser.ruleNames[tree.getRuleIndex()])
+build_anytree(root, tree)
+
+# Imprime el árbol anytree
+for pre, fill, node in RenderTree(root):
+    print(f"{pre}{node.name}")
+
+# Genera una representación visual del árbol anytree
+DotExporter(root).to_picture("tree.png")
