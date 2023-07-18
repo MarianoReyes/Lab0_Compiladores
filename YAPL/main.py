0809f02df846d57646ac30e0e04d8207ce2d3a2f
MarianoReyes <josemarianoreyesh02@gmail.com>
2023-07-18 16:54:05 -0600

    manejo de errores

--- YAPL/main.py
+++ YAPL/main.py
@@ -11,10 +11,13 @@ from antlr4.tree.Tree import TerminalNode
 
 
 class CustomErrorListener(antlr4.error.ErrorListener.ErrorListener):
+    def __init__(self):
+        self.errors = []
+
     def syntaxError(self, recognizer, offendingSymbol, line, column, msg, e):
         # Customize the error message here
         custom_msg = f"Error en la linea {line}, columna {column}, mensaje: {msg}"
-        print(custom_msg)
+        self.errors.append(custom_msg)
 
     def reportAmbiguity(self, recognizer, dfa, startIndex, stopIndex, exact, ambigAlts, configs):
         pass
@@ -25,9 +28,14 @@ class CustomErrorListener(antlr4.error.ErrorListener.ErrorListener):
     def reportContextSensitivity(self, recognizer, dfa, startIndex, stopIndex, prediction, configs):
         pass
 
+    def get_errors(self):
+        return self.errors
+
+
 root = Tk()
 root.withdraw()
-input_file = filedialog.askopenfilename(initialdir=os.getcwd(),filetypes=(('YAPL files', '*.yapl'),('All files', '*.*')))
+input_file = filedialog.askopenfilename(initialdir=os.getcwd(
+), filetypes=(('YAPL files', '*.yapl'), ('All files', '*.*')))
 with open(input_file, 'r') as file:
     input_data = file.read()
 input_stream = InputStream(input_data)
@@ -42,31 +50,38 @@ parser.removeErrorListeners()
 parser.addErrorListener(error_listener)
 
 # Aplica la regla inicial de la gramática (expr)
-tree = parser.program()# Linea a cambiar en funcion de la regla inicial del parser
+tree = parser.program()  # Linea a cambiar en funcion de la regla inicial del parser
 
+# Retrieve all errors after parsing
+errors = error_listener.get_errors()
+for error in errors:
+    print(error)
 
-print(tree.toStringTree(recog=parser))
+if errors:
+    print("----------------------------------------------------------------------------------")
+    print("\nYa que hay 1 o más errores no se armará el árbol sintáctico del archivo input.\n")
+    print("----------------------------------------------------------------------------------")
+else:
+    print(tree.toStringTree(recog=parser))
 
+    def build_anytree(node, antlr_node):
+        if isinstance(antlr_node, TerminalNode):
+            value = antlr_node.getText()
+            Node(value, parent=node)
+        else:
+            rule_name = parser.ruleNames[antlr_node.getRuleIndex()]
+            child_node = Node(rule_name, parent=node)
+            for child in antlr_node.getChildren():
+                build_anytree(child_node, child)
 
-def build_anytree(node, antlr_node):
-    if isinstance(antlr_node, TerminalNode):
-        value = antlr_node.getText()
-        Node(value, parent=node)
-    else:
-        rule_name = parser.ruleNames[antlr_node.getRuleIndex()]
-        child_node = Node(rule_name, parent=node)
-        for child in antlr_node.getChildren():
-            build_anytree(child_node, child)
+    root = Node(parser.ruleNames[tree.getRuleIndex()])
+    build_anytree(root, tree)
 
+    # Imprime el árbol anytree
+    for pre, fill, node in RenderTree(root):
+        print(f'{pre}{node.name}')
 
-root = Node(parser.ruleNames[tree.getRuleIndex()])
-build_anytree(root, tree)
-
-# Imprime el árbol anytree
-for pre, fill, node in RenderTree(root):
-    print(f'{pre}{node.name}')
-
-# Genera una representación visual del árbol anytree
-dot_exporter = UniqueDotExporter(root)
-dot_exporter.to_picture("visual_tree.png")
-os.system(f"start visual_tree.png")
+    # Genera una representación visual del árbol anytree
+    dot_exporter = UniqueDotExporter(root)
+    dot_exporter.to_picture("visual_tree.png")
+    os.system(f"start visual_tree.png")
